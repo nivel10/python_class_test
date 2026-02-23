96dacf188046ad71d1d61b0848d298fb90fdb638
T.S.U. - Ing. Carlos H <carlos.e.herrera.j@gmail.com>
2026-02-23 17:55:00 -0400

    Sync - 02/23/2026 - 17:54

--- main_class.py
+++ main_class.py
@@ -43,16 +43,21 @@ if __name__ == '__main__':
             print('Do you want to create a new person?')
             print('y = yes')
             print('n = no')
+            #region old code
             # print('Please select an option (y / n), and press enter.')
+            #endregion old code
             print('Please select an option, and press enter.')
             input_option = input()
 
         if input_option.lower() != 'y' and input_option.lower() != 'yes':
             program_end = True
+            print(' ')
         else:
             first_name: str = input('Please enter the first name: ')
             last_name: str = input('Please enter the last name: ')
+            #region old code
             # birthday: datetime = datetime.strptime(input('Please enter the birthday (YYYY-MM-DD)'), '%Y-%m-%d')
+            #endregion old code
             birthday: datetime = datetime.strptime(input('Please enter the birthday (YYY-MM-DD): '), '%Y-%m-%d')
             has_hair: bool = input('Please enter the has hair (y = yes, n = no): ')
             if has_hair.lower() == 'y' or has_hair.lower() == 'yes':
@@ -62,12 +67,19 @@ if __name__ == '__main__':
                 has_hair = False
                 hair_color: str = None
             
-            gender: str = input(f'Please enter the gender ({PERSON_GENDERS.male}, {PERSON_GENDERS.female}, {PERSON_GENDERS.other}): ')
-            gender = gender.lower().strip()
-            while gender != PERSON_GENDERS.male and gender != PERSON_GENDERS.female and  gender != PERSON_GENDERS.other:
-                gender: str = input(f'Please enter the gender ({PERSON_GENDERS.male}, {PERSON_GENDERS.female}, {PERSON_GENDERS.other}): ')
-                gender = gender.lower().strip()
-
+            #region old code
+            # gender: str = input(f'Please enter the gender ({PERSON_GENDERS.male}, {PERSON_GENDERS.female}, {PERSON_GENDERS.other}): ')
+            # gender = gender.lower().strip()
+            # while gender != PERSON_GENDERS.male and gender != PERSON_GENDERS.female and  gender != PERSON_GENDERS.other:
+            #     gender: str = input(f'Please enter the gender ({PERSON_GENDERS.male}, {PERSON_GENDERS.female}, {PERSON_GENDERS.other}): ')
+            #     gender = gender.lower().strip()
+            #endregion old code
+            allowed_genders: list[str] = [value for key, value in vars(PERSON_GENDERS).items() if not key.startswith('_')]
+            gender_prompt: str = f'Please enter the gender ({', '.join(allowed_genders)}): '
+            while (gender := input(gender_prompt).lower().strip()) not in allowed_genders:
+                print('')
+                print(f'Gender {gender} is not allowed, please try again.')
+            
             person: Person = Person(
                 first_name=first_name,
                 last_name=last_name,
@@ -80,18 +92,26 @@ if __name__ == '__main__':
             is_first_person = False
 
     for person in persons:
+        #region old code
         # print('----------------------')
+        #endregion old code
         print('-' * 22)
+        #region old code
         # print(f'first name: {person.first_name}')
         # print(f'last name: {person.last_name}')
+        #endregion old code
         print(f'full name: {person.full_name}')
         print(f'gender: {person.gender}')
+        #region old code
         # print(f'age: {person.get_age()}')
+        #endregion old code
         print(f'age: {person.age}')
         print(f'birthday: {person.birthday}')
         print(f'birthday str: {person.birthday_str}')
         print(f'hair: {person.hair}')
+        #region old code
         # print('----------------------')
+        #endregion old code
         print('')
 
     print('end process')
